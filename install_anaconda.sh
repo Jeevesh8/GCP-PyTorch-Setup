ba8e433fc1f28339d960b0f2cfae809e82775d4d
Jeevesh8 <48825663+Jeevesh8@users.noreply.github.com>
2020-05-11 15:44:33 +0530

    Fixed Small Bug

--- install_anaconda.sh
+++ install_anaconda.sh
@@ -1,4 +1,4 @@
 pip install python3.7
 wget https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh
-bash ~/Anaconda3-2020.02-Linux-x86_64.sh
+bash ./Anaconda3-2020.02-Linux-x86_64.sh
 sudo reboot
